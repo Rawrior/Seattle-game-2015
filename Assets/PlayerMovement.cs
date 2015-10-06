f0e92ae715d0258312a77ec58b36cf647419b97c
unknown <rune2040@VID3C970EAF02B7.djes.dk>
2015-10-06 14:23:31 -0700

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movement();
+	}
+
+    void movement()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)* 10 * Time.deltaTime);
+    }
+}
