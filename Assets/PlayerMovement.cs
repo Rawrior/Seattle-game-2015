fb4054fea22c05edd21d0e801c0fec60d1cc020a
unknown <rune2040@VID3C970EAF02B7.djes.dk>
2015-10-07 15:38:48 -0700

    Multiple player compatibility succesful

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,11 +11,14 @@ public class PlayerMovement : MonoBehaviour
     public float jumpDuration;
     public float jumpHight = 3.5f;
     public float jumpSpeed;
+    public bool airborne;
+    public float jumpTime;
 
     public string Horizontal;
 	// Use this for initialization
 	void Start ()
     {
+        jumpTime = 0.25f;
         playerPlaying = "P" + tag[gameObject.tag.Length - 1];
         whoIsPlaying();
         Debug.Log(playerPlaying);
@@ -27,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+
+        if (airborne == true)
+        {
+            jumpDuration += Time.deltaTime;
+        }
         Jump();
         movement(Horizontal);
 	}
@@ -37,36 +45,36 @@ public class PlayerMovement : MonoBehaviour
     }
     void Jump()
     {
-        if ((player1Playing == true) && (Input.GetKey(KeyCode.Joystick1Button4)) && jumpDuration <= 0.25f)
+        if ((player1Playing == true) && (Input.GetKey(KeyCode.Joystick2Button4)) && jumpDuration <= jumpTime)
         {
             jumpSpeed = 5f;
+            airborne = true;
             GetComponent<Rigidbody2D>().velocity = (new Vector2(0, jumpSpeed));
-            jumpDuration += Time.deltaTime;
             Debug.Log("P1 jumping");
             //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
         }
-        else if ((player2Playing == true) && (Input.GetKey(KeyCode.Joystick2Button4)) && jumpDuration <= 0.25f)
+        else if ((player2Playing == true) && (Input.GetKey(KeyCode.Joystick3Button4)) && jumpDuration <= jumpTime)
         {
             jumpSpeed = 5f;
+            airborne = true;
             GetComponent<Rigidbody2D>().velocity = (new Vector2(0, jumpSpeed));
-            jumpDuration += Time.deltaTime;
             Debug.Log("P2 jumping");
             //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
         }
-        else if ((player3Playing == true) && (Input.GetKey(KeyCode.Joystick3Button4)) && jumpDuration <= 0.25f)
+        else if ((player3Playing == true) && (Input.GetKey(KeyCode.Joystick4Button4)) && jumpDuration <= jumpTime)
         {
             jumpSpeed = 5f;
+            airborne = true;
             GetComponent<Rigidbody2D>().velocity = (new Vector2(0, jumpSpeed));
-            jumpDuration += Time.deltaTime;
             Debug.Log("P3 jumping");
             //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
         }
-        else if ((player4Playing == true) && (Input.GetKey(KeyCode.Joystick4Button4)) && jumpDuration <= 0.25f)
+        else if ((player4Playing == true) && (Input.GetKey(KeyCode.Joystick5Button4)) && jumpDuration <= jumpTime)
         {
             jumpSpeed = 5f;
+            airborne = true;
             GetComponent<Rigidbody2D>().velocity = (new Vector2(0, jumpSpeed));
-            jumpDuration += Time.deltaTime;
-            Debug.Log(" P4jumping");
+            Debug.Log(" P4 jumping");
             //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
         }
         else
@@ -97,6 +105,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (other.CompareTag("Ground"))
         {
+            airborne = false;
             jumpDuration = 0;
         }
     }
