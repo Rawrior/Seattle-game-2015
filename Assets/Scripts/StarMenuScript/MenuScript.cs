a4c6993ec262afc151f3d153aa6dcb9b8796bca9
unknown <rune2040@VID3C970EAF02B7.djes.dk>
2015-10-19 15:54:06 -0700

    start menu 49% done

--- Assets/Scripts/StarMenuScript/MenuScript.cs
+++ Assets/Scripts/StarMenuScript/MenuScript.cs
@@ -21,6 +21,7 @@ public class MenuScript : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        Debug.Log(Input.GetAxis(vertical));
 
         currentLevel = Application.loadedLevel;
         if (currentLevel == 0)
@@ -31,6 +32,10 @@ public class MenuScript : MonoBehaviour
         {
             menuTwo();
         }
+        else if (currentLevel == 1)
+        {
+            menuCredits();
+        }
 
 	}
     void menuOne()
@@ -39,7 +44,7 @@ public class MenuScript : MonoBehaviour
         {
             Application.LoadLevel(2);
         }
-        if (playSelected == true && Input.GetAxis(vertical) <= 0)
+        if (playSelected == true && Input.GetAxis(vertical) <= -1)
         {
             playSelected = false;
             creditsSelected = true;
@@ -48,12 +53,12 @@ public class MenuScript : MonoBehaviour
         {
             Application.LoadLevel(1);
         }
-        else if (creditsSelected == true && Input.GetAxis(vertical) >= 0)
+        else if (creditsSelected == true && Input.GetAxis(vertical) >= 1 * Time.deltaTime)
         {
             creditsSelected = false;
             playSelected = true;
         }
-        else if (creditsSelected && Input.GetAxis(vertical) <= 0)
+        else if (creditsSelected && Input.GetAxis(vertical) <= -1 * Time.deltaTime)
         {
             creditsSelected = false;
             exitSelected = true;
@@ -86,19 +91,19 @@ public class MenuScript : MonoBehaviour
         {
             Application.LoadLevel(3);
         }
-        if ((players2Selected == true && Input.GetAxis(vertical) <= 0) || (players4Selected == true && Input.GetAxis(vertical) >= 0))
+        if ((players2Selected == true && Input.GetAxis(vertical) <= -1) || (players4Selected == true && Input.GetAxis(vertical) >= 1))
         {
             players3Selected = true;
         }
-        else if ((players3Selected == true && Input.GetAxis(vertical) <= 0) || (backSelected == true && Input.GetAxis(vertical) >= 0))
+        else if ((players3Selected == true && Input.GetAxis(vertical) <= -1) || (backSelected == true && Input.GetAxis(vertical) >= 1))
         {
             players4Selected = true;
         }
-        if (players4Selected == true && Input.GetAxis(vertical) <= 0)
+        if (players4Selected == true && Input.GetAxis(vertical) <= 1)
         {
             backSelected = true;
         }
-        if (players3Selected == true && Input.GetAxis(vertical) >= 0)
+        if (players3Selected == true && Input.GetAxis(vertical) >= -1)
         {
             players2Selected = true;
         }
@@ -107,5 +112,14 @@ public class MenuScript : MonoBehaviour
             Application.LoadLevel(0);
         }
 
+    }
+
+    void menuCredits()
+    {
+        backSelected = true;
+        if (backSelected == true && Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            Application.LoadLevel(0);
+        }
     }
 }
