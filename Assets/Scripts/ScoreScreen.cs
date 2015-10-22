48e530c6a08d1ff20e4b9c56584aa6c3e5647139
unknown <rune2040@VID3C970EAF02B7.djes.dk>
2015-10-22 16:00:34 -0700

    end score screen fixed

--- Assets/Scripts/ScoreScreen.cs
+++ Assets/Scripts/ScoreScreen.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class ScoreScreen : MonoBehaviour {
+public class ScoreScreen : MonoBehaviour
+{
+    public Text Player1Score;
+    public Text Player2Score;
+    public Text Player3Score;
+    public Text Player4Score;
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
@@ -22,5 +30,13 @@ public class ScoreScreen : MonoBehaviour {
         {
             Application.LoadLevel(0);
         }
+        PlayerScores();
 	}
+    void PlayerScores()
+    {
+        Player1Score.text = "Player 1 end score: " + PlayersPlaying.player1Points;
+        Player2Score.text = "Player 2 end score: " + PlayersPlaying.player2Points;
+        Player3Score.text = "Player 3 end score: " + PlayersPlaying.player3Points;
+        Player4Score.text = "Player 4 end score: " + PlayersPlaying.player4Points;
+    }
 }
